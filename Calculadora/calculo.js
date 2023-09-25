cb6c8eacd317c485c4053f141be83dea14aead6f
NidereMaluco <130380825+NidereMaluco@users.noreply.github.com>
2023-09-25 08:55:00 -0300

    Add files via upload

---
+++ Calculadora/calculo.js
@@ -0,0 +1,10 @@
+let nota1 = document.querySelector(".nota1");
+let nota1 = nota1.textContent;
+
+let nota2 = document.querySelector(".nota2");
+let nota2 = nota2.textContent;
+
+let nota3 = document.querySelector(".nota3");
+let nota3 = nota3.textContent;
+
+
